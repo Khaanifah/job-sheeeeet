a17feff00166ec257c2f61b4e0ddc4dc833483b0
Khaanifah <itsnaankhaanifaah.com>
2026-09-17 07:45:19 +0700

    bagian 26, revisi dikit

--- js/data.js
+++ js/data.js
@@ -1,7 +1,42 @@
 export const initialProducts = [
-    { id: 1, title: "Laptop Gaming", price: 1200, category: "laptops", rating: 4.5, stock: 10, thumbnail: "https://dummyjson.com/image/150" },
-    { id: 2, title: "Smartphone Pro", price: 800, category: "phones", rating: 4.2, stock: 15, thumbnail: "https://dummyjson.com/image/150" },
-    { id: 2, title: "Smartphone Pro", price: 800, category: "phones", rating: 4.2, stock: 15, thumbnail: "https://dummyjson.com/image/150" },
-    { id: 4, title: "Smartwatch Sport", price: 250, category: "wearables", rating: 4.6, stock: 20, thumbnail: "https://dummyjson.com/image/150" },
-    { id: 5, title: "Mechanical Keyboard", price: 90, category: "accessories", rating: 4.3, stock: 5, thumbnail: "https://dummyjson.com/image/150" }
+    {
+    id: 1,
+    title: "Laptop Gaming",
+    price: 1200,
+    category: "laptops",
+    rating: 4.5,
+    thumbnail: "https://dummyjson.com/image/200x200"
+  },
+  {
+    id: 2,
+    title: "Smartphone Pro",
+    price: 800,
+    category: "phones",
+    rating: 4.2,
+    thumbnail: "https://dummyjson.com/image/200x200"
+  },
+  {
+    id: 3,
+    title: "Wireless Headphones",
+    price: 100,
+    category: "audio",
+    rating: 4.0,
+    thumbnail: "https://dummyjson.com/image/200x200"
+  },
+  {
+    id: 4,
+    title: "Smartwatch Sport",
+    price: 250,
+    category: "wearables",
+    rating: 4.6,
+    thumbnail: "https://dummyjson.com/image/200x200"
+  },
+  {
+    id: 5,
+    title: "Mechanical Keyboard",
+    price: 90,
+    category: "accessories",
+    rating: 4.3,
+    thumbnail: "https://dummyjson.com/image/200x200"
+  }
 ];
